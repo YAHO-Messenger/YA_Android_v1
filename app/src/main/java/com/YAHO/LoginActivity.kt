25bed110521aba7f45c2812ea22d7d70cdd3bfe7
Leesin0222 <dragon02229@naver.com>
2021-10-14 18:41:20 +0900

    🔧 :: 로그인에서 회원가입 넘어가는 구현

--- app/src/main/java/com/YAHO/LoginActivity.kt
+++ app/src/main/java/com/YAHO/LoginActivity.kt
@@ -1,11 +1,20 @@
 package com.YAHO
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.YAHO.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.noAccount.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
     }
 }
