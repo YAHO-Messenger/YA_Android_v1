687e1936b144b2531ab787dfbbf62ccbe9279f81
Leesin0222 <dragon02229@naver.com>
2021-10-26 19:57:22 +0900

    🔧 :: have_account클릭 시 로그인 화면으로 이동

--- app/src/main/java/com/YAHO/RegisterActivity.kt
+++ app/src/main/java/com/YAHO/RegisterActivity.kt
@@ -1,11 +1,23 @@
 package com.YAHO
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.YAHO.databinding.ActivityLoginBinding
+import com.YAHO.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.noAccount.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }
