34618a1e73869e3d3cd119207676961beedc3ec3
Leesin0222 <dragon02229@naver.com>
2021-10-28 19:43:17 +0900

    🔧 :: Meowbottomnavigation과 프레그먼트 연결

--- app/src/main/java/com/YAHO/MainActivity.kt
+++ app/src/main/java/com/YAHO/MainActivity.kt
@@ -2,8 +2,10 @@ package com.YAHO
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.YAHO.databinding.ActivityMainBinding
 import com.YAHO.databinding.ActivityRegisterBinding
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,8 +17,47 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        addFragment(FreeFragment.newInstance())
+        binding.bottomNavigation.show(0)
+
+        binding.bottomNavigation.add(MeowBottomNavigation.Model(0,R.drawable.ic_baseline_developer_mode_24))
+        binding.bottomNavigation.add(MeowBottomNavigation.Model(1,R.drawable.ic_round_fastfood_24))
+        binding.bottomNavigation.add(MeowBottomNavigation.Model(2,R.drawable.ic_baseline_whatshot_24))
+        binding.bottomNavigation.add(MeowBottomNavigation.Model(3,R.drawable.ic_baseline_sports_esports_24))
+        binding.bottomNavigation.add(MeowBottomNavigation.Model(4,R.drawable.ic_outline_movie_24))
+
+        binding.bottomNavigation.setOnClickMenuListener {
+            when(it.id){
+                0 -> {
+                    replaceFragment(DevelopFragment.newInstance())
+                }
+                1 -> {
+                    replaceFragment(FoodFragment.newInstance())
+                }
+                2 -> {
+                    replaceFragment(FreeFragment.newInstance())
+                }
+                3 -> {
+                    replaceFragment(GameFragment.newInstance())
+                }
+                4-> {
+                    replaceFragment(MovieFragment.newInstance())
+                }
+            }
+        }
     }
+
+
     override fun onBackPressed() {
         // super.onBackPressed()
+    }
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentTransition = supportFragmentManager.beginTransaction()
+        fragmentTransition.replace(R.id.fragmentContainer,fragment).addToBackStack(Fragment::class.java.simpleName).commit()
+
+    }
+    private fun addFragment(fragment: Fragment){
+        val fragmentTransition = supportFragmentManager.beginTransaction()
+        fragmentTransition.add(R.id.fragmentContainer,fragment).addToBackStack(Fragment::class.java.simpleName).commit()
     }
 }
