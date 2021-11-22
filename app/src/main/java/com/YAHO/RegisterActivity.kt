23fc5becb00bc47ec51180ac7532262e052d92f7
Leesin0222 <dragon02229@naver.com>
2021-11-22 12:10:14 +0900

    📑 :: 회원가입 구현

--- app/src/main/java/com/YAHO/RegisterActivity.kt
+++ app/src/main/java/com/YAHO/RegisterActivity.kt
@@ -3,18 +3,53 @@ package com.YAHO
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.YAHO.databinding.ActivityRegisterBinding
+import com.google.gson.GsonBuilder
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.POST
 
 
 class RegisterActivity : AppCompatActivity() {
-    //ViewBinding
-    private lateinit var  binding: ActivityRegisterBinding
-
+    private lateinit var binding: ActivityRegisterBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        var gson = GsonBuilder().setLenient().create()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://15.165.28.181:8080")
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+
+        val service = retrofit.create(RegisterService::class.java)
+
+
+        binding.registerBtn.setOnClickListener {
+            val remail = binding.idEt.toString()
+            val rpassword = binding.passwordEt.toString()
+            service.Register(Login(remail, rpassword)).enqueue(object : Callback<String> {
+                override fun onResponse(call: Call<String>, response: Response<String>) {
+                    val result = response.body()
+                    Log.e("회원가입", "${result}")
+                }
+                override fun onFailure(call: Call<String>, t: Throwable) {
+                    Log.e("회원가입", "${t.localizedMessage}")
+                }
+            })
+        }
     }
+}
+
+interface RegisterService {
+    @POST("/Register")
+    fun Register(@Body login: Login) : Call<String>
 }
